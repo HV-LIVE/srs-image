7d575b904c1a4f8cd46a00b16e6f1a4c1f481370
张添 <zhangtian@mail.hdzuoye.com>
2023-08-04 18:12:35 +0800

    Feat: 创建项目

---
+++ image/README.md
@@ -0,0 +1,5 @@
+# 编译镜像
+
+```
+docker build -t hvlive/srs:latest --no-cache .
+```
---
+++ image/launcher/launch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -o errexit
+
+work_dir="$(dirname $(readlink -f $0))"
+cd "$work_dir"
+
+echo "setup config"
+# TODO
+# python3 "$work_dir/setup.py"
+
+# 回到原镜像设定的工作目录
+cd "/usr/local/srs"
+
+echo "launch srs"
+exec "/usr/local/srs/objs/srs" "-c" "/usr/local/srs/conf/custom.conf"
