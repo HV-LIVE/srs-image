f6613ecccfa5e58d2366e06bba028377fde87184
zhangtian <zhangtian1993@hotmail.com>
2023-08-19 15:13:34 +0800

    Dev: 完善功能

--- image/launcher/launch.sh
+++ image/launcher/launch.sh
@@ -5,8 +5,7 @@ work_dir="$(dirname $(readlink -f $0))"
 cd "$work_dir"
 
 echo "setup config"
-# TODO
-# python3 "$work_dir/setup.py"
+python3 "$work_dir/setup.py"
 
 # 回到原镜像设定的工作目录
 cd "/usr/local/srs"
